d01eb0618e49b9668f14950ebd456e499325cdc5
gogowonji <65698313+gogowonji@users.noreply.github.com>
2021-11-26 13:05:01 +0900

    Initial commit

---
+++ app/src/main/java/com/example/mediaplayerpractice/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.mediaplayerpractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mp = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startResAudio(View v){
+        mp = MediaPlayer.create(this, R.raw.old_pop);
+        mp.start();
+
+    }
+
+
+    public void stopResAudio(View v){
+        if(mp!=null){
+            mp.stop();
+            mp.release();
+        }
+        mp=null;
+    }
+}
